ec0bb50303dbb1661f04805d44798bba971448e8
t0mmusic <92428577+t0mmusic@users.noreply.github.com>
2021-12-21 12:32:26 +1030

    Post piscine 100% complete

--- C_00/ex08/ft_print_combn.c
+++ C_00/ex08/ft_print_combn.c
@@ -17,32 +17,82 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_combn(int n)
+int	*limarray(int *lim, int n)
 {
 	int	i;
 	int	j;
-	int str[n - 1];
+
+	i = 0;
+	j = n;
+	while (i < n)
+	{
+		lim[i] = 10 - j;
+		i++;
+		j--;	
+	}
+	lim[i] = '\0';
+	return (lim);
+}
+
+int	*nbarray(int *nb, int n)
+{
+	int	i;
 
 	i = 0;
 	while (i < n)
 	{
-		str[i] = i + '0';
-		ft_putchar(str[i]);
+		nb[i] = i;
 		i++;
 	}
-	str[i] = '\0';
-	j = 0;
-	while (str[j] )
+	nb[i] = '\0';
+	return (nb);
+}
+
+void	printnb(int *nb, int n, int this, int that)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
 	{
+		ft_putchar(nb[i] + '0');
+		i++;
+	}
+	if (this != that)
+		write(1, ", ", 2);
+}
+
+void	ft_print_combn(int n)
+{
+	int	i;
+	int	nb[n];
+	int	lim[n];
+
+	nbarray(nb, n);
+	limarray(lim, n);
+	printnb(nb, n, nb[0], lim[0]);
+	i = 0;
+	while (nb[0] != lim[0])
+	{
+		if (nb[i + 1] != lim[i + 1] && nb[i + 1])
+			i++;
+		else
+		{
+			nb[i] = nb[i] + 1;
+			while (nb[i + 1])
+			{
+				nb[i + 1] = nb[i] + 1;
+				i++;
+			}
+			printnb(nb, n, nb[0], lim[0]);
+			i = 0;
+		}
 
 	}
 }
 
 int	main(void)
 {
-	int i;
-
-	i = 8;
-	ft_print_combn(i);
+	ft_print_combn(10);
 	return (0);
 }
